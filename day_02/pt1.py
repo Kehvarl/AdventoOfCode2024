e9aedac1ddcb96fe7bed18745d86895c9c116170
Kehvarl <Kehvarl@Kehvarl.com>
2024-12-01 22:50:04 -0700

    Day 02 solved

--- day_02/pt1.py
+++ day_02/pt1.py
@@ -5,6 +5,29 @@ with open("input.txt") as f:
     content = [x.strip() for x in f.readlines()]
     # content = [int(x) for x in f.readlines()]
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+safes = 0
+for line in content:
+    levels = [int(x) for x in line.split()]
+    safe = True
+    prev = levels[0]
+    direction = 0
+    for level in levels [1:]:
+        if direction == 0:
+            if level > prev:
+                direction = 1
+            else:
+                direction = -1
+        else:
+            if (level > prev and direction == -1) or (level < prev and direction == 1):
+                safe = False
+        if (abs(level - prev)) > 3 or (abs(level - prev)) < 1:
+            safe = False
+        if not safe:
+            break
+        prev = level
+    if safe:
+        safes += 1
+
+print(safes)
+
+
--- day_02/pt2.py
+++ day_02/pt2.py
@@ -1,12 +1,50 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    content = [x.strip() for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+def test(levels):
+    safe = True
+    fault = 0
+    prev = levels[0]
+    direction = 0
+    for i, level in enumerate(levels[1:]):
+        if direction == 0:
+            if level > prev:
+                direction = 1
+            else:
+                direction = -1
+        else:
+            if (level > prev and direction == -1) or (level < prev and direction == 1):
+                safe = False
+                fault = i + 1
+        if (abs(level - prev)) > 3 or (abs(level - prev)) < 1:
+            safe = False
+            fault = i + 1
+        if not safe:
+            break
+        prev = level
+    return safe, fault
+
+safes = 0
+for line in content:
+    retest = True
+    levels = [int(x) for x in line.split()]
+    safe, fault = test(levels)
+    if safe:
+        safes += 1
+    else:
+        copy = levels.copy()
+        for x, val in enumerate(levels):
+            l = copy.copy()
+            del l[x]
+            safe, fault = test(l)
+            print(levels, l, fault)
+            if safe:
+                safes += 1
+                break
+
+print(safes)
