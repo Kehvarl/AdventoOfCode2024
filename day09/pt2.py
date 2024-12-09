b6a2b5ddff49fadbf48752df69c73873124020b1
Kehvarl <Kehvarl@Kehvarl.com>
2024-12-08 22:59:35 -0700

    Day 08 1971 / 2467

--- day09/pt2.py
+++ day09/pt2.py
@@ -1,12 +1,51 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    content = [x.strip() for x in f.readlines()][0]
+    # content = [int(x) for x in f.readlines()]
 
+files = []
+file_ids = {}
+empty = []
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+file = True
+fi = 0
+for y, v1 in enumerate(content):
+    if file:
+        file_ids[fi] = int(v1)
+        files.extend([fi]*int(v1))
+        file = False
+    else:
+        empty.append((len(files), int(v1)))
+        files.extend(['.']*int(v1))
+        file = True
+        fi += 1
+
+idx = len(files) - 1
+while idx >= 0:
+    if files[idx] != '.':
+        file_len = file_ids[files[idx]]
+        empty_index = None
+        for ei, e in enumerate(empty):
+            if e[1] >= file_len and e[0] < idx:
+                empty_index = ei
+                break
+        if empty_index is not None:
+            i, s = empty.pop(empty_index)
+            for f in range(file_len):
+                files[i] = files[idx - f]
+                files[idx - f] = '.'
+                i += 1
+            if file_len < s:
+                empty.insert(empty_index, (i, s-file_len))
+        idx -= file_len
+    else:
+        idx -= 1
+
+# print(files)
+cs = 0
+for y, v1 in enumerate(files):
+    if v1 != '.':
+        cs += (y * int(v1))
+print(cs)
