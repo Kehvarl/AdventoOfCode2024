5d7d946801197b4665fcfbac6370f800fab34f74
Kehvarl <Kehvarl@Kehvarl.com>
2024-12-09 22:30:15 -0700

    Day 10 3898 / 3716

--- day10/pt1.py
+++ day10/pt1.py
@@ -5,6 +5,46 @@ with open("input.txt") as f:
     content = [x.strip() for x in f.readlines()]
     # content = [int(x) for x in f.readlines()]
 
+trailheads = []
+endpoints = []
+trailmap = {}
+
 for y, v1 in enumerate(content):
     for x, v2 in enumerate(v1):
-        pass
+        if v2 != '.':
+            trailmap[(x, y)] = int(v2)
+        if v2 == '.':
+            continue
+        elif int(v2) == 0:
+            trailheads.append((x, y))
+        elif int(v2) == 9:
+            endpoints.append((x, y))
+
+
+def walk(trailhead, trailmap):
+    score = 0
+    bfs_q = deque()
+    bfs_q.append(trailhead)
+    visited = set()
+
+    while len(bfs_q) > 0:
+        x, y = bfs_q.popleft()
+        curval = trailmap[(x, y)]
+        if curval == 9 and (x, y) not in visited:
+            score += 1
+        visited.add((x, y))
+
+        for nx, ny in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            if (x + nx, y + ny) not in visited and trailmap.get((x + nx, y + ny)) == curval + 1:
+                #print((x, y, curval), (x + nx, y + ny, trailmap.get((x + nx, y + ny))))
+                bfs_q.append((x + nx, y + ny))
+
+    return score
+
+
+score = 0
+for t in trailheads:
+    s = walk(t, trailmap)
+    score += s
+
+print(score)
