764506081afe82385802255d4027b904563ef8a5
Kehvarl <Kehvarl@Kehvarl.com>
2024-12-10 22:44:48 -0700

    Day 11 2522 / 3363

--- day11/pt1.py
+++ day11/pt1.py
@@ -2,9 +2,29 @@ from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = [x.strip() for x in f.readlines()]
+    content = [x.strip() for x in f.readlines()][0]
     # content = [int(x) for x in f.readlines()]
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+stones = [int(x) for x in content.split(" ")]
+
+
+def step(stones):
+    new_stones = []
+    for stone in stones:
+        if stone == 0:
+            new_stones.append(1)
+        elif len(str(stone)) % 2 == 0:
+            s = str(stone)
+            l = s[0:len(s)//2]
+            r = s[len(s)//2:]
+            new_stones.append(int(l))
+            new_stones.append(int(r))
+        else:
+            new_stones.append((stone * 2024))
+    return new_stones
+
+print(stones)
+for i in range(25):
+    stones = step(stones)
+    print(stones)
+print(len(stones))
--- day11/pt2.py
+++ day11/pt2.py
@@ -1,12 +1,41 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    content = [x.strip() for x in f.readlines()][0]
+    # content = [int(x) for x in f.readlines()]
+
+stones = defaultdict(int)
+
+for s in content.split(" "):
+    stones[int(s)] += 1
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+def step(stone):
+    if stone == 0:
+        return [1]
+    elif len(str(stone)) % 2 == 0:
+        s = str(stone)
+        l = s[0:len(s)//2]
+        r = s[len(s)//2:]
+        return [int(l), int(r)]
+    else:
+        return[int(stone)*2024]
+
+
+def fast_step(stones):
+    new_stones = defaultdict(int)
+    for s in stones:
+        s_count = stones[s]
+        for r in step(s):
+            new_stones[r] += s_count
+    return new_stones
+
+
+for i in range(75):
+    stones = fast_step(stones)
+
+sum = 0
+for s in stones:
+    sum += stones[s]
+print(sum)
