4652a4dea4d4232556668be9a5db94fba0c12682
Kehvarl <Kehvarl@Kehvarl.com>
2024-12-12 22:39:47 -0700

    Day 13 3784 / 1349

--- day13/pt1.py
+++ day13/pt1.py
@@ -2,9 +2,35 @@ from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = [x.strip() for x in f.readlines()]
+    content = f.read().strip().split("\n\n")
+    # content = [x.strip() for x in f.readlines()]
     # content = [int(x) for x in f.readlines()]
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+machines = []
+for m in content:
+    a, b, p = m.split("\n")
+    ax, ay = a[9:].strip().split(", ")
+    bx, by = b[9:].strip().split(", ")
+    px, py = p[7:].strip().split(", ")
+    ba = (int(ax[2:]), int(ay[2:]))
+    bb = (int(bx[2:]), int(by[2:]))
+    pr = (int(px[2:]), int(py[2:]))
+
+    machines.append([ba, bb, pr])
+
+a_tokens = 0
+b_tokens = 0
+for ba, bb, pr in machines:
+    ax, ay = ba
+    bx, by = bb
+    px, py = pr
+    a = (px * by - py * bx) / (ax * by - ay * bx)
+    if a != int(a):
+        continue
+
+    b = (py - ay * a) // by
+    if a == int(a) and b == int(b):
+        a_tokens += int(a)
+        b_tokens += int(b)
+
+print(a_tokens * 3 + b_tokens)
--- day13/pt2.py
+++ day13/pt2.py
@@ -1,12 +1,37 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    content = f.read().strip().split("\n\n")
+    # content = [x.strip() for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
 
+offset = 10000000000000
+machines = []
+for m in content:
+    a, b, p = m.split("\n")
+    ax, ay = a[9:].strip().split(", ")
+    bx, by = b[9:].strip().split(", ")
+    px, py = p[7:].strip().split(", ")
+    ba = (int(ax[2:]), int(ay[2:]))
+    bb = (int(bx[2:]), int(by[2:]))
+    pr = (int(px[2:]) + offset, int(py[2:]) + offset)
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+    machines.append([ba, bb, pr])
+
+a_tokens = 0
+b_tokens = 0
+for ba, bb, pr in machines:
+    ax, ay = ba
+    bx, by = bb
+    px, py = pr
+    a = (px * by - py * bx) / (ax * by - ay * bx)
+    if a != int(a):
+        continue
+
+    b = (py - ay * a) // by
+    if a == int(a) and b == int(b):
+        a_tokens += int(a)
+        b_tokens += int(b)
+
+print(a_tokens * 3 + b_tokens)
