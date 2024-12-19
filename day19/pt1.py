19262b6926c6210dc6143c69416853ba1f8e6c78
Kehvarl <Kehvarl@Kehvarl.com>
2024-12-18 22:23:33 -0700

    Day 19 complete

--- day19/pt1.py
+++ day19/pt1.py
@@ -1,10 +1,30 @@
 from collections import defaultdict, deque
 from pprint import pprint
+from queue import Queue
 
 with open("input.txt") as f:
-    content = [x.strip() for x in f.readlines()]
+    towels, patterns = f.read().split("\n\n")
+    # content = [x.strip() for x in f.readlines()]
     # content = [int(x) for x in f.readlines()]
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+towels = towels.split(", ")
+
+designs = [d.strip() for d in patterns.strip().split("\n")]
+
+
+def test(design, towels):
+    for t in towels:
+        if design == t:
+            return True
+        if design[:len(t)] == t and test(design[len(t):], towels):
+            return True
+    return False
+
+
+possible = 0
+for d in designs:
+    if test(d, towels):
+        possible += 1
+print(possible)
+
+print(possible)
