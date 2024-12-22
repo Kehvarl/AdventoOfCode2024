6768bcac36f692a920392651f5abb3723e340ce5
Kehvarl <Kehvarl@Kehvarl.com>
2024-12-22 00:19:41 -0700

    Day 22 1717/3352

--- day22/pt1.py
+++ day22/pt1.py
@@ -2,9 +2,23 @@ from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = [x.strip() for x in f.readlines()]
+    content = [int(x.strip()) for x in f.readlines()]
     # content = [int(x) for x in f.readlines()]
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+
+def next_secret(secret):
+    ns = ((secret * 64) ^ secret) % 16777216
+    ns = ((ns // 32) ^ ns) % 16777216
+    ns = ((ns * 2048) ^ ns) % 16777216
+    return ns
+
+secrets = []
+for s in content:
+    ns = s
+    for i in range(2000):
+        ns = next_secret(ns)
+        if s == 1 and ns == 8685429:
+            print(i)
+    secrets.append(ns)
+print(secrets)
+print(sum(secrets))
