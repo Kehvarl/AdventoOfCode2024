3cb969fd3953d92b195a0f182c2cf59512b98371
Kehvarl <Kehvarl@Kehvarl.com>
2024-12-24 22:19:31 -0700

    Day 25 1036 / 860

--- day25/pt1.py
+++ day25/pt1.py
@@ -2,9 +2,51 @@ from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = [x.strip() for x in f.readlines()]
+    content = [x.strip().split("\n") for x in f.read().split("\n\n")]
     # content = [int(x) for x in f.readlines()]
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+#print(content)
+
+locks = []
+lock_codes = []
+keys = []
+key_codes = []
+for schematic in content:
+    if schematic[0] == "#####" and schematic[-1] == ".....":
+        locks.append(schematic)
+        cols = [0,0,0,0,0]
+        for line in schematic[1:]:
+            for i, char in enumerate(line):
+                if char == "#":
+                    cols[i] += 1
+        lock_codes.append(cols)
+    elif schematic[0] == "....." and schematic[-1] == "#####":
+        keys.append(schematic)
+        cols = [0,0,0,0,0]
+        for line in schematic[:-1]:
+            for i, char in enumerate(line):
+                if char == "#":
+                    cols[i] += 1
+        key_codes.append(cols)
+
+matches = defaultdict(list)
+match_count = 0
+for lock in lock_codes:
+    for key in key_codes:
+        good = True
+        for t,l in enumerate(key):
+            if l + lock[t] > 5:
+                good = False
+                break
+        if good:
+            print(lock, key)
+            match_count += 1
+            matches[tuple(lock)].append(key)
+
+print(matches)
+print(match_count)
+#print(locks)
+#print(lock_codes)
+#print(keys)
+#print(key_codes)
+#print(matches)
